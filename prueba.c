338e367077528c63ddcc94a8f56bcb317f0e9063
Administrador <gabriel.gaboz123@gmail.com>
2022-07-06 02:02:17 -0400

    Opcion para ver saldo y correcciones
    Agregue la opcion para ver saldo, cambie un poco la estetica y saltos de linea, se suma o resta saldo si ganas y comente muchas lineas

--- prueba.c
+++ prueba.c
@@ -14,8 +14,10 @@
 #define b 04 
 #define c 05 
 
-//Validación
-int validar_numero(char numero[]), N = 20;
+
+int validar_numero(char numero[]);//Validación
+int N = 20;
+float apuesta;
 char *symbols [5] = { "(" , "!", "#", "$", "%" }; //Simbolos
 
 main(){
@@ -25,86 +27,89 @@ main(){
 	int  apuestaopc, intentos = 0; //variables del juego
 	int opc, num, randNumber, index, diaspartidas=0; //Roll
 	char *linea[3];//espacios en el roll
-	//Dinero
-	float dineroT, dineroEn, dineroD; //time_t tiempoahora
+	float dineroT, dineroEn, dineroD; //Dinero
+	
 	//Archivo
 	FILE *archivomoney;
 	archivomoney=fopen("archivomoney.txt","a+"); //Se abre el archivo
-	fprintf(archivomoney, "%s\n"); //,ctime(&tiempoahora) 
+	fprintf(archivomoney, "%s\n"); 
 	
-	
-	randNumber = random_int();//rand() ; //el numero de vueltas
-	num= random_char();
+	//Inicializacion de numeros aleatoreos
+	randNumber = random_int();//el numero de vueltas
+	num= random_char();//el numero del simbolo en el array
 	do{		
-			menu:
-			linea[0] = "";
-			linea[1] = "";
-			linea[2] = "";
-			printf("===========================================\n");
-			printf("\t Maquina tragamonedas!\n");
-			printf("===========================================\n");	
-			printf("\t	 Bienvenido \n");
-			printf("Al tragamonedas del casino de la USM \n");
-			printf("===========================================\n");
+		menu:
+		linea[0] = "";//espacios
+		linea[1] = "";//espacios
+		linea[2] = "";//espacios
+		printf("===========================================\n");
+		printf("\t Maquina tragamonedas!\n");
+		printf("===========================================\n");	
+		printf("\t	 Bienvenido \n");
+		printf("    Al tragamonedas del casino de la USM \n");
+		printf("===========================================\n");
 			
-			//menu de inicio
-			printf("\t Menu \n");
-			printf("1. Iniciar juego \n");
-			printf("2. Ver como se juega\n");
-			printf("3. Ver registro de la maquina\n");
-			printf("4. Salir \n");
-			printf("Ingrese una opcion del menu: ");
-			scanf("%s", numero);	
-			n=validar_numero(numero);
-			system("cls");
+		//menu de inicio
+		printf("\t\t Menu \n");
+		printf("\t1. Iniciar juego \n");
+		printf("\t2. Ver como se juega\n");
+		printf("\t3. Ver registro de la maquina\n");
+		printf("\t4. Ver saldo disponible en la maquina\n");
+		printf("\t5. Salir \n");
+		printf("===========================================\n");
+		printf("Ingrese una opcion del menu: ");
+		scanf("%s", numero);	
+		n=validar_numero(numero);//valida el numero
+		system("cls");
 			
-			numerovalido=atoi(numero);
+		numerovalido=atoi(numero);//Llamada al metodo para la validación
 				
-			if(numerovalido > 4){
-				system("cls");
-				printf("Opcion fuera de rango, ingrese una opcion valida \n");
-				system("pause");
-				system("cls");
-			}
+		if(numerovalido > 5){
+			system("cls");
+			printf("Opcion fuera de rango, ingrese una opcion valida \n");
+			system("pause");
+			system("cls");
+		}
 		
 		//Menu del juego
 		switch(numerovalido){
-			case 1:	printf("SU APUESTA \n");
-					printf("Ingrese la cantidad para apostar: \n");
+			case 1:	printf("===========================================\n");
+					printf("\tSU APUESTA \n");
+					printf("===========================================\n");
+					printf("Ingrese la cantidad para apostar: ");
 					scanf("%f", &dineroD);
+					printf("$");
 					system("cls");
-					printf("SU APUESTA \n");
-					printf("Su saldo es de: %.2f \n\n", dineroD);
+					printf("===========================================\n");
+					printf("\tSU APUESTA ES DE \n");
+					printf("\t     %.2f$ \n",dineroD);
+					printf("===========================================\n");
+					apuesta = dineroD;//Intercambio
+					system("pause");
 					system("cls");
 					diaspartidas ++;
+					
 					//Roll
-					for (i = 0; i < 3; i++)
+					for (i = 0; i < 3; i++) //Hace las columnas filas
 					{
-						//printf("%d", randNumber);
-						//index = 0;
 						for (j = 0; j < randNumber; j++) //numero de giros que hará 
 						{
-							//if (k > 5 );
-							//index = 0;
-							//linea += symbols[k]; //index
 							printf("===========================================\n");
 							printf("\tINICIANDO EL GIRO...\n");
 							printf("===========================================\n");
-							printf("\t%s\t%s\t%s",symbols[random_char()],symbols[random_char()],symbols[random_char()]); //linea
-							Sleep(75); //Tiempo para cada roll
+							printf("\t%s\t%s\t%s",symbols[random_char()],symbols[random_char()],symbols[random_char()]);//Roll de los simbolos
+							Sleep(55); //Tiempo para cada roll
 							system("cls");
 						}
-		
 						linea[i] = symbols[num];
-		
-						num= random_char(); //el numero segun la posición del simbolo
-						randNumber = random_int(); //el numero de vueltas
+						num= random_char(); //llamada al metodo el numero segun la posición del simbolo
+						randNumber = random_int(); //llamada al metodo el numero de vueltas 
 					}
 					printf("===========================================\n");
 					printf("\tResultado del giro \n");
-					printf("\t%s\t%s\t%s\n", linea[0], linea[1], linea[2]); //linea, lineas
+					printf("\t%s\t%s\t%s\n", linea[0], linea[1], linea[2]); //Impresión de los simbolos según el espacio del vector
 					printf("===========================================\n");
-					matchwin(linea);
+					matchwin(linea);//llamada al metodo de validación de si coincidieron los simbolos
 					system("pause");
 					system("cls");
 					break;
@@ -129,8 +134,17 @@ main(){
 					system("cls");
 					break;
 					
-			case 4: //SALIDA
-					printf("Saliendo...");
+			case 4: printf("===========================================\n");
+					printf("\tSU SALDO ES DE \n");
+					printf("\t     %.2f$ \n",apuesta);
+					printf("===========================================\n");
+					system("pause");
+					system("cls");
+					break;
+							
+			case 5: //SALIDA
+					salida:
+					printf("Saliendo...\n");
 					system("pause");
 					goto end;
 			
@@ -138,16 +152,23 @@ main(){
 		}
 		
 	}while(opc == 1);
-	printf("Quiere volver a menu? \n");
-	printf("1. Si \n");
-	printf("2. No \n");
-	printf("Ingrese una de las opciones: ");
+	//Vuelta al menu si el usuario lo desea
+	printf("===========================================\n");
+	printf("\tQuiere volver a menu? \n");
+	printf("\t1. Si \n");
+	printf("\t2. No \n");
+	printf("===========================================\n");
+	printf("\tIngrese una de las opciones: ");
 	scanf("%d", &opc);
 	system("cls");
 	if(opc == 1){
 		opc = 0;
 		goto menu;
 	}
+	if(opc == 2){
+		goto salida;
+	}
+	
 	end: 
 	return 0;	
 	
@@ -160,7 +181,7 @@ int validar_numero(char numero[]){
 	for(i=0; i<strlen(numero); i++){
 		if(!(isdigit(numero[i]))){ //condicional de si la variable ingresada es diferente de un numero entero como i, enviar mensaje de error
 			system("cls");
-			printf("Ingresa un solo numero \n");
+			printf("Ingresa un solo numeros \n");
 			system("pause");
 			system("cls");
 			return 0;
@@ -169,51 +190,46 @@ int validar_numero(char numero[]){
 	return 1;
 }
 
-//Metodo un minimo y un maximo
+//Metodo un minimo y un maximo para la cantidad de vueltas
 int random_int()
 {
 	int value = 0;
 	int r = 0;
 	while(1 == 1){
-		srand(time(NULL));
+		srand(time(NULL)); //uso de la libreria Time
 		r = rand() % N;
-		//printf("%d \n", r);
 		if( r > 0 && r < N  ){
 			value = r;
 			break; 
 		}
 	}
 	return value;
-   //return min + rand() % (max+1 - min);
 }
 
-//Metodo random char
+//Metodo random char en el array
 int random_char()
 {
-	//int len= sizeof(symbols) / sizeof(char);//Calcular el tamaño del valor del simbolo
 	int value = 0;
 	int r = 0;
 	while(1 == 1){
-		srand(time(NULL));
+		srand(time(NULL)); //uso de la libreria Time
 		r = rand() % 5 ;
-		//printf("%d \n", r);
 		if( r < 5 ){
 			value = r;
 			break; 
 		}
 	}
 	return value;
-   //return min + rand() % (max+1 - min);
 }
 
+//Metodo de busqueda de los simbolos
 int busqueda(char **linea){
-	
 	int n = 5, match, i, j;
-	for(i = 0; i < n;i++){
+	for(i = 0; i < n;i++){ //hace un conteo con un iterador
 		int count = 0;
-		char *character = symbols[i];
+		char *character = symbols[i]; //puntero
 		for(j=0; j < 3;j++){
-			if (strcmp(character,linea[j])==0){
+			if (strcmp(character,linea[j])==0){ //comparación
 				count++;
 			}			
 		}
@@ -224,16 +240,19 @@ int busqueda(char **linea){
 	return match;
 }
 
+//Metodo vacio para los resultados del match
 void matchwin(char **resultado) {
 	int matches = busqueda(resultado);
 
 		if (matches == 1 || matches == 2)
 		{
 			printf("Intenta de nuevo \n");
+			apuesta -= apuesta;
 		}
 		
 		if (matches > 3)
 		{
 			printf("Lechuo! ganaste \n");
+			apuesta += apuesta;
 		}
 }
